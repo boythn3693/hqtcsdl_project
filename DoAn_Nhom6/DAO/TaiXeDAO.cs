ad3fb7d335cfb447ea09bcc8a5cf7b387330194f
boythn3693 <mr.dailc@gmail.com>
2017-09-02 11:37:47 +0700

    no message

--- DoAn_Nhom6/DAO/TaiXeDAO.cs
+++ DoAn_Nhom6/DAO/TaiXeDAO.cs
@@ -1,15 +1,119 @@
-﻿using System;
+﻿using DTO;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DAO
 {
     public class TaiXeDAO
     {
+        public void SuaTaiXe(TaiXeDTO dto)
+        {
+            SqlConnection cn;
+            cn = DataProviders.ConnectionDB();
+            SqlCommand cmd = new SqlCommand("sp_CAPNHAT_TAIXE", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@MaTaiXe", SqlDbType.Int);
+            cmd.Parameters.Add("@TenTaiXe", SqlDbType.NVarChar);
+            cmd.Parameters.Add("@KhaNangLai", SqlDbType.BigInt);
+            cmd.Parameters.Add("@QuangDuong", SqlDbType.BigInt);
+            cmd.Parameters.Add("@TenDangNhap", SqlDbType.VarChar);
+            cmd.Parameters.Add("@MatKhau", SqlDbType.VarChar);
+            cmd.Parameters.Add("@TinhTrang", SqlDbType.Bit);
+            cmd.Parameters.Add("@MaTo", SqlDbType.Int);
+            //gán giá trị cho tham số
+            cmd.Parameters["@MaTaiXe"].Value = dto.Mataixe;
+            cmd.Parameters["@TenTaiXe"].Value = dto.Tentaixe;
+            cmd.Parameters["@KhaNangLai"].Value = dto.Khanang;
+            cmd.Parameters["@QuangDuong"].Value = dto.Quangduong;
+            cmd.Parameters["@TenDangNhap"].Value = dto.Tendangnhap;
+            cmd.Parameters["@MatKhau"].Value = dto.Matkhau;
+            cmd.Parameters["@TinhTrang"].Value = dto.Tinhtrang;
+            cmd.Parameters["@MaTo"].Value = dto.Mato;
+            //Thực thi câu truy vấn
+            cmd.ExecuteNonQuery();
+            //Đóng kết nối
+            cn.Close();
+        }
+
+        public void ThemTaiXe(TaiXeDTO dto)
+        {
+            SqlConnection cn;
+            cn = DataProviders.ConnectionDB();
+            SqlCommand cmd = new SqlCommand("sp_THEM_TAIXE", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@TenTaiXe", SqlDbType.NVarChar);
+            cmd.Parameters.Add("@KhaNangLai", SqlDbType.BigInt);
+            cmd.Parameters.Add("@QuangDuong", SqlDbType.BigInt);
+            cmd.Parameters.Add("@TenDangNhap", SqlDbType.VarChar);
+            cmd.Parameters.Add("@MatKhau", SqlDbType.VarChar);
+            cmd.Parameters.Add("@TinhTrang", SqlDbType.Bit);
+            cmd.Parameters.Add("@MaTo", SqlDbType.Int);
+            cmd.Parameters.Add("@MaTaiXe", SqlDbType.Int).Direction = ParameterDirection.Output;
+
+            //gán giá trị cho tham số
+            cmd.Parameters["@TenTaiXe"].Value = dto.Tentaixe;
+            cmd.Parameters["@KhaNangLai"].Value = dto.Khanang;
+            cmd.Parameters["@QuangDuong"].Value = dto.Quangduong;
+            cmd.Parameters["@TenDangNhap"].Value = dto.Tendangnhap;
+            cmd.Parameters["@MatKhau"].Value = dto.Matkhau;
+            cmd.Parameters["@TinhTrang"].Value = dto.Tinhtrang;
+            cmd.Parameters["@MaTo"].Value = dto.Mato;
+            //Thực thi câu truy vấn
+            cmd.ExecuteNonQuery();
+            //lấy mã sp đc phát sinh sau khi thêm, gán lại vào dto
+            dto.Mataixe = Convert.ToInt32(cmd.Parameters["@MaTaiXe"].Value);
+            //Đóng kết nối
+            cn.Close();
+        }
+
+        public List<TaiXeDTO> TimKiemTaiXe( string tentaixe )
+        {
+            SqlConnection cn;
+            cn = DataProviders.ConnectionDB();
+            SqlCommand cmd = new SqlCommand("sp_TIMKIEM_TAIXE", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@TENTAIXE", SqlDbType.NVarChar);
+            //gán giá trị cho tham số
+            cmd.Parameters["@Keys"].Value = tentaixe;
+            //Thực thi câu truy vấn
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<TaiXeDTO> list = new List<TaiXeDTO>();
+            while (reader.Read())
+            {
+                TaiXeDTO dto = new TaiXeDTO();
+
+                dto.Mataixe = reader.GetInt32(0);
+                dto.Tentaixe = reader.GetString(1);
+                dto.Khanang = reader.GetInt64(2);
+                dto.Quangduong = reader.GetInt64(3);
+                dto.Tendangnhap = reader.GetString(4);
+                dto.Tinhtrang = reader.GetBoolean(6);
+                dto.Mato = reader.GetInt32(7);
+
+                list.Add(dto);
+            }
+            //Đóng kết nối
+            cn.Close();
+            return list;
+        }
+
+        public DataTable DanhSachTaiXe()
+        {
+            SqlConnection cn;
+            cn = DataProviders.ConnectionDB();
+            SqlCommand cmd = new SqlCommand("sp_DANHSACH_TAIXE", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            cmd.Dispose();
+            da.Dispose();
+            dt.Dispose();
+            return dt;
+        }
+
         public int DangNhap(string tendn, string matkhau)
         {
             SqlConnection cn;
--- DoAn_Nhom6/GUI/frmMain.Designer.cs
+++ DoAn_Nhom6/GUI/frmMain.Designer.cs
@@ -30,15 +30,59 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.quảnLýToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lịchTrìnhToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.phânCôngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lịchTrìnhToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.phânCôngToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quảnLýToolStripMenuItem,
+            this.lịchTrìnhToolStripMenuItem,
+            this.lịchTrìnhToolStripMenuItem1});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(787, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
+            // 
+            // quảnLýToolStripMenuItem
+            // 
+            this.quảnLýToolStripMenuItem.Name = "quảnLýToolStripMenuItem";
+            this.quảnLýToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.quảnLýToolStripMenuItem.Text = "Quản lý";
+            // 
+            // lịchTrìnhToolStripMenuItem
+            // 
+            this.lịchTrìnhToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.phânCôngToolStripMenuItem});
+            this.lịchTrìnhToolStripMenuItem.Name = "lịchTrìnhToolStripMenuItem";
+            this.lịchTrìnhToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.lịchTrìnhToolStripMenuItem.Text = "Quản trị";
+            // 
+            // phânCôngToolStripMenuItem
+            // 
+            this.phânCôngToolStripMenuItem.Name = "phânCôngToolStripMenuItem";
+            this.phânCôngToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.phânCôngToolStripMenuItem.Text = "Phân công";
+            // 
+            // lịchTrìnhToolStripMenuItem1
+            // 
+            this.lịchTrìnhToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.phânCôngToolStripMenuItem1});
+            this.lịchTrìnhToolStripMenuItem1.Name = "lịchTrìnhToolStripMenuItem1";
+            this.lịchTrìnhToolStripMenuItem1.Size = new System.Drawing.Size(69, 20);
+            this.lịchTrìnhToolStripMenuItem1.Text = "Lịch trình";
+            // 
+            // phânCôngToolStripMenuItem1
+            // 
+            this.phânCôngToolStripMenuItem1.Name = "phânCôngToolStripMenuItem1";
+            this.phânCôngToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.phânCôngToolStripMenuItem1.Text = "Phân công";
             // 
             // frmMain
             // 
@@ -51,6 +95,8 @@
             this.Name = "frmMain";
             this.Text = "Quản lý lịch trình Xe khách";
             this.Load += new System.EventHandler(this.frmMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,5 +105,10 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem quảnLýToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lịchTrìnhToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem phânCôngToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lịchTrìnhToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem phânCôngToolStripMenuItem1;
     }
 }
